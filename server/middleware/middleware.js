fa8a8062f959a2d78cca374f3e0feaa82d3420a5
Shivam singh <79458284+shivam8496@users.noreply.github.com>
2025-02-17 18:37:31 +0530

    Revert "made virtual to security"
    This reverts commit 1a0a6666e412f2151a1591030faf6c8383605d4a.

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -1,24 +1,25 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();  // Load environment variables
+const jwt  = require('jsonwebtoken');
+const secret = "superS3cr3t1";
 
-const secret = process.env.JWT_SECRET; // Fetch secret from .env
-
-function generateToken(data) {
-    return jwt.sign(data, secret);
+function generateToken (data){
+      
+    return jwt.sign(data,secret);
 }
 
-function jwtAuthenticate(req, res, next) {
+function jwtAuthenticate (req, res ,next){
     var data = req.headers.authorize;
-    if (data) {
-        var token = data.split(' ')[1];
-        jwt.verify(token, secret, (err, original) => {
-            if (err) return res.status(403).json({ message: "error occurred" });
-            req.user = original;
-            next();
-        });
-    } else {
-        res.status(404).json({ message: "wrong bearer Token" });
+    if(data){
+      var token = data.split(' ')[1];
+      jwt.verify(token,secret,(err,original)=>{
+        if(err) return res.status(403).json({message:"error occured"});
+         else{req.user = original;
+         next();}
+      });
     }
+    else {
+      res.status(404).json({message:"wrong bearer Token"});
+    }
+  
 }
 
-module.exports = { generateToken, jwtAuthenticate };
+module.exports = {generateToken,jwtAuthenticate};
